d8e5ee1cb11af575a78e50c1044719687d3cfeeb
sadegh_sargazi <138787095+sadeghsk1379@users.noreply.github.com>
2023-07-14 13:57:51 +0330

    Updatehospital.py
    Now in this update we have surgery class

--- hospital/hospital.py
+++ hospital/hospital.py
@@ -1,20 +1,35 @@
-from departments import Department
-from patients import Patient
-from staff import Staff
-
+from patients  import Patient
+from  staff import Doctor,Nurse,Staff
+from appointments import Appointment
+from surgery import Surgery
 
 class Hospital:
 
-    def __init__(self, name, address, phone, website):
+    def __init__(self, name):
         self.name = name
-        self.address = address
-        self.phone = phone
-        self.website = website
-        self.department = []
+        self.patients = []
+        self.staff = []
+        self.appointments = []
+        self.surgerys=[]
+    
+    def add_patient(self, patient):
+        self.patients.append(patient)
+        
+    def add_staff(self, staff_member):
+        self.staff.append(staff_member)
 
-    def add_department(self, name_of_department):
-        self.department.append(name_of_department)
-
+    def book_appointment(self, patient, doctor, appointment_date, appointment_time):
+        appointment = Appointment(patient, doctor, appointment_date, appointment_time)
+        self.appointments.append(appointment)
+        print("Appointment booked for {} on {} at {}".format(patient.name, appointment_date, appointment_time))
+        
+    def surgery(self,patient,doctor,kind_of_surgery,cost_of_surgery):
+        surgery= Surgery(kind_of_surgery,cost_of_surgery,doctor,patient)
+        self.surgerys.append(surgery)
+        print("Surgery done for {} by {} costs {}".format(patient.name,doctor.name,surgery.cost_of_surgery))
+        
+        
+        
 # Example usage        
 my_hospital = Hospital("City Hospital")
 
@@ -25,3 +40,6 @@ mary = Doctor("Mary Smith", "Cardiology")
 my_hospital.add_staff(mary)
 
 my_hospital.book_appointment(john, mary, "2019-12-01", "10:00")
+
+frank=Patient("frank","Zabol","++12345678")
+my_hospital.surgery(frank,mary,"Heart",17500)
